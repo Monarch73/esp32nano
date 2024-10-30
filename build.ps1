63ba67df8a12b1411ca800fda3e0af401948ce4a
Niels <niels@monarch.de>
2024-10-30 23:55:43 +0100

    add esp32 build

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-make -f .\makeFile\makeEspArduino.mk ESP_ROOT=./hardware/esp32 BOARD=NANO32 FLASH_DEF=4M2M SKETCH=./src/main.ino
+make -f .\makeFile\makeEspArduino.mk CHIP=esp32 ESP_ROOT=./hardware/esp32 BOARD=NANO32 FLASH_DEF=4M2M SKETCH=./src/main.ino
